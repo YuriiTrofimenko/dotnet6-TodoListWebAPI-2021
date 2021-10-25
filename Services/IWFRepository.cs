483ed1fda015d6858fc93bab3ed34d9bd142719f
YuriiTrofimenko <tyaa@ukr.net>
2021-10-25 19:24:10 +0300

    Setup WF Repository Interface

--- Services/IWFRepository.cs
+++ Services/IWFRepository.cs
@@ -1,7 +1,10 @@
+using System.Linq;
+using TodoListWebAPI.Models;
+
 namespace TodoListWebAPI.Services
 {
     public class IWFRepository
     {
-        
+        public IQueryable<ToDoItem> Items { get; }
     }
 }
